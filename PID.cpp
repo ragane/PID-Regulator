7f5e262d093525410a3ad3e05d188e0bb30d375c
ragane27 <ragane27@gmail.com>
2022-10-01 16:45:45 +0200

    updated main file

--- PID.cpp
+++ PID.cpp
@@ -1,6 +1,10 @@
-#include "Arduino.h"
-#include "PID_h"
+#include "PID.h"
 
+#if (ARDUINO >= 100)
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
 
 PID::PID()
 {
@@ -10,10 +14,11 @@ PID::PID()
     this->last_error = 0;
     this->last_input = 0;
     this->I_error = 0;
+}
 
-PID::PID(float Kp, float Ki,  Kd)
+PID::PID(float Kp, float Ki, float Kd)
 {
-    PID::tune_PID(Kp, Ki, Kd)
+    PID::tune_PID(Kp, Ki, Kd);
     PID::set_limits(0, 255);
     
     this->last_error = 0;
@@ -21,14 +26,14 @@ PID::PID(float Kp, float Ki,  Kd)
     this->I_error = 0;
 }
 
-PID::tune_PID(float Kp, float Ki, float Kd)
+void PID::tune_PID(float Kp, float Ki, float Kd)
 {
     this->KP = Kp;
     this->KP = Ki;
     this->KD = Kd;
 }
 
-PID::set_limits(float MIN_VAL, float MAX_VAL)
+void PID::set_limits(int MIN_VAL, int MAX_VAL)
 {
     if (MIN_VAL >= MAX_VAL)
         MIN_VAL = -MIN_VAL;
@@ -37,7 +42,7 @@ PID::set_limits(float MIN_VAL, float MAX_VAL)
         MAX_VAL = -MAX_VAL;        
 }
 
-PID::compute(float input_val, float target_val, float dt
+float PID::compute(float input_val, float target_val, float dt)
 {
     float error = target_val - input_val;
 
@@ -54,7 +59,7 @@ PID::compute(float input_val, float target_val, float dt
     // 
     float result = (this->KP * error) + (this->KI * I_error) + (this->KD * D_error);
   
-    result = result > max_val ? max_val : result;
-    result = result < min_val ? min_val : result; 
+    result = result > (float)max_val ? (float)max_val : result;
+    result = result < (float)min_val ? (float)min_val : result; 
     return result;
 }
